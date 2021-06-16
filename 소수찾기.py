9efd6f7abc487fa37acb4256c110700b0ce5e8a0
Kim-TaeUk <xodnr0130@naver.com>
2021-06-16 21:49:46 +0900

    Jun 16,2021

---
+++ 소수찾기.py
@@ -0,0 +1,13 @@
+# def is_prime_number(num):
+#     for i in range(2, num):
+#         if num % i == 0:
+#             return False
+#     return True 
+
+# def solution(n):
+#     answer = 0
+#     for k in range(2,n+1):
+#         if is_prime_number(k) == True:
+#             answer += 1
+#     return answer
+# time complexity떄문에 실패 뜨는 code
